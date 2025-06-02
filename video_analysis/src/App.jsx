78d62dc5017176bd9695b731b09077d7f1599689
Rayan Roshan <2006rayanroshan@gmail.com>
2025-06-01 22:05:19 -0400

    API situation

--- video_analysis/src/App.jsx
+++ video_analysis/src/App.jsx
@@ -40,7 +40,8 @@ export default function App() {
     setError('');
     
     try {
-      const genAI = new GoogleGenerativeAI("");
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const genAI = new GoogleGenerativeAI(apiKey);
       const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-preview-05-20" });
       
       const result = await model.generateContent([
